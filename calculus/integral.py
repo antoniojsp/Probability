ea73d409e2e1302e802cc47fdf0970eeb2f0e961
Antonio Silva Paucar <antonios@uoregon.edu>
2023-04-04 06:46:22 -0700

    fix tests, compatible windows and os x

--- calculus/integral.py
+++ calculus/integral.py
@@ -1,4 +1,4 @@
-from math import pi, sin
+from math import pi, sin, cos
 import sys
 
 sys.path.append('../')
@@ -31,34 +31,43 @@ def area_under_curve(f, dx, end, start=0):
     i = start
     answer = []
     while i < end:
+        # print(i, area)
         area += dx * f(i)
         answer.append(dx * f(i))
+        if area >0.5:
+            print(i)
+            break
         i += dx
 
     x_arr = [i for i in range(0, len(answer))]
 
-    graph(x_arr, answer)
+    # graph(x_arr, answer)
 
     return area
 
+def formula2(x):
+    return 1/6*x+1/3
+
+def formula_cereal(x):
+    return x*(x/200 - 39/20)
 
 # print(area_under_curve(inverse_parabola, 1.414, 0.00001))
-# print(area_under_curve(continuous_function1, 0.00001, 410, 404))
-import matplotlib.pyplot as plt
-import numpy as np
-
-plt.style.use('_mpl-gallery')
-
-# make data
-x = np.linspace(0, 10, 100)
-y = 4 + 2 * np.sin(2 * x)
-
-# plot
-fig, ax = plt.subplots()
-
-ax.plot(x, y, linewidth=2.0)
-
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
-
-plt.show()
+print(area_under_curve(formula_cereal, 0.00001, 410, 390))
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# plt.style.use('_mpl-gallery')
+#
+# # make data
+# x = np.linspace(0, 10, 100)
+# y = 4 + 2 * np.sin(2 * x)
+#
+# # plot
+# fig, ax = plt.subplots()
+#
+# ax.plot(x, y, linewidth=2.0)
+#
+# ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+#        ylim=(0, 8), yticks=np.arange(1, 8))
+#
+# plt.show()
