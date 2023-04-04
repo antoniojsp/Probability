a22bab5f4445735d05f64967810cfdfa9b9878af
antonio Silva <antonios@uoregon.edu>
2023-04-03 18:37:56 -0700

    add test

--- calculus/integral.py
+++ calculus/integral.py
@@ -1,5 +1,9 @@
-# formula = x**2
-from math import pi
+from math import pi, sin
+import sys
+
+sys.path.append('../')
+
+from calculations.graph import graph
 
 
 def perimeter_circle(r):
@@ -11,21 +15,50 @@ def parabola(x):
 
 
 def inverse_parabola(x):
-    return -1*(x**2) + 2
+    return -1 * (x ** 2) + 2
 
 
 def integral_parabola(end, start=0):
     return (end ** 3) / 3 - (start ** 3) / 3
 
 
-def area_under_curve(f, end, dx):
+def continuous_function1(x):
+    return (x / 200) - (39 / 20)
+
+
+def area_under_curve(f, dx, end, start=0):
     area = 0
-    i = 0
+    i = start
+    answer = []
     while i < end:
         area += dx * f(i)
+        answer.append(dx * f(i))
         i += dx
+
+    x_arr = [i for i in range(0, len(answer))]
+
+    graph(x_arr, answer)
 
     return area
 
 
-print(area_under_curve(inverse_parabola, 1.414, 0.00001))
+# print(area_under_curve(inverse_parabola, 1.414, 0.00001))
+# print(area_under_curve(continuous_function1, 0.00001, 410, 404))
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('_mpl-gallery')
+
+# make data
+x = np.linspace(0, 10, 100)
+y = 4 + 2 * np.sin(2 * x)
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
