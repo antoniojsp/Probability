72f24a1b356e23b8b72e013032c2b29c62bc8bc8
antonio Silva <antonios@uoregon.edu>
2023-04-05 06:14:06 -0700

    separate

--- calculus/integral.py
+++ calculus/integral.py
@@ -6,53 +6,31 @@ sys.path.append('../')
 from calculations.graph import graph
 
 
-def perimeter_circle(r):
-    return 2 * pi * r
-
-
-def parabola(x):
-    return x ** 2
-
-
-def inverse_parabola(x):
-    return -1 * (x ** 2) + 2
-
-
-def integral_parabola(end, start=0):
-    return (end ** 3) / 3 - (start ** 3) / 3
-
-
-def continuous_function1(x):
-    return (x / 200) - (39 / 20)
-
-
 def area_under_curve(f, dx, end, start=0):
+    area = 0
+    i = start
+    while i < end:
+        area += dx * f(i)
+        i += dx
+
+    return area
+
+
+def area_under_curve_graph(f, dx, end, start=0):
     area = 0
     i = start
     answer = []
     while i < end:
-        # print(i, area)
         area += dx * f(i)
         answer.append(dx * f(i))
-        # if area >0.5:
-        #     print(i)
-        #     break
         i += dx
 
     x_arr = [i for i in range(0, len(answer))]
-
-    # graph(x_arr, answer)
-
+    graph(x_arr, answer)
     return area
 
-def formula2(x):
-    return 1/6*x+1/3
-
-def formula_cereal(x):
-    return ((x**2)/200 - (39/20)*x)
-
 # print(area_under_curve(inverse_parabola, 1.414, 0.00001))
-print(area_under_curve(formula_cereal, 0.00001, 410, 390))
+# print(area_under_curve(formula_cereal, 0.00001, 410, 390))
 # import matplotlib.pyplot as plt
 # import numpy as np
 #
---
+++ scratchpaper/area_under_curve_sample.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.path.append('../')
+from calculus.integral import area_under_curve
+from math import sin, cos, pi
+
+
+def perimeter_circle(r):
+    return 2 * pi * r
+
+
+def parabola(x):
+    return x ** 2
+
+
+def inverse_parabola(x):
+    return -1 * (x ** 2) + 2
+
+
+def integral_parabola(end, start=0):
+    return (end ** 3) / 3 - (start ** 3) / 3
+
+
+def continuous_function1(x):
+    return (x / 200) - (39 / 20)
+
+
+def formula2(x):
+    return 1 / 6 * x + 1 / 3
+
+
+def formula_cereal(x):
+    return ((x ** 2) / 200 - (39 / 20) * x)
+
+
+print(area_under_curve(formula_cereal, 0.00001, 410, 390))
