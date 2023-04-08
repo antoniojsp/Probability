df2d6262135c4a18b6402da45b31102160ac297e
Antonio Silva Paucar <antonios@uoregon.edu>
2023-04-08 14:03:07 -0700

    fix tests, compatible windows and os x

--- calculations/continuous_variance_sd.py
+++ calculations/continuous_variance_sd.py
@@ -19,7 +19,7 @@ def standard_deviation(f, end, start=0):
 def standard_deviation2(f, end, start=0):
     expected_val = expected_value_continuous(f, end, start)
     f_2 = lambda x: x**2 * f(x)
-    e_x_2 = area_under_curve(f_2, 0.0001, end=end, start=start)
-    return sqrt(e_x_2 - (expected_val ** 2))
+    expected_val_square = area_under_curve(f_2, 0.0001, end=end, start=start)
+    return sqrt(expected_val_square - (expected_val ** 2))
 
 
