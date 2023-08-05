292dc15bf7b78459c08b5c8e22da419383acd3c4
Antonio Silva Paucar <antonios@uoregon.edu>
2023-08-05 13:00:38 -0700

    first chapter

--- current_london/testing_sum.py
+++ current_london/testing_sum.py
@@ -1,17 +1,11 @@
-arr_x = [7,3,1,0,-6]
-arr_y = [-3, 5,-8, 9, 1]
-func = lambda x: 2*x
+arr_x = [1,3,2,4,10]
 
-def sigma(f, lower, upper, arr, arr_used)->float:
+def sigma(f, lower, upper, arr)->float:
     result = 0
     for i in range(lower-1, upper):
-        print(arr[i])
         result += f(arr[i])
-
     return result
 
 print("###RESULT###")
-func_2 = lambda x: 4*(x-1)
-func_3 = lambda x, y: x**2+2*(y**2)
-arr_used = [arr_y, arr_x]
-print(sigma(func_3, 1, 3, arr_used))
+func_2 = lambda x: 2*(x)**2
+print(sigma(func_2, 1, 3, arr_x))
